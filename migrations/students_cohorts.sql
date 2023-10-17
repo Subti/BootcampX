0f54dcd18f887d27bc3c7822cabe2f42d5ab3e74
JP Saliba <jpsaliba@outlook.com>
2023-10-17 11:08:23 -0400

    Fixed wrong types in students_cohorts for ids, and added teachers and assistance_requests tables along with fake data to fill them.

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,12 +1,12 @@
 CREATE TABLE cohorts (
-  id INT PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   start_date DATE,
   end_date DATE
 );
 
 CREATE TABLE students (
-  id INT PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255),
   phone VARCHAR(32),
